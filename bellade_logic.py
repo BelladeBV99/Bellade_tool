c19f62ce2652a7191c4cbe381d560afa13aedc70
BelladeBV99 <info@bellade.be>
2025-05-07 13:22:50 +0200

    Update bellade_logic.py

--- bellade_logic.py
+++ bellade_logic.py
@@ -1,57 +1,71 @@
 from typing import Dict
-import numpy as np
 
 def bereken_rendement(data: Dict[str, float]) -> Dict:
     vermogen = data['vermogen']
     opstartkost = data['opstartkost']
     huurprijs = data['huurprijs']
+    huurprijs_later = data['huurprijs_later']
     starttarief = data['starttarief']
-    kwh_prijs = data['kwhprijs']
+    kwhprijs = data['kwhprijs']
     parkeertarief = 1
-    laadsessies_per_dag = data['laadsessies']
-    kostprijs_energie = data['kostprijsenergie']
+    initiële_laadsessies = data['laadsessies']
+    kostprijsenergie = data['kostprijsenergie']
     maandgroei = data['maandstijging']
+    verbruik_per_sessie = data['verbruik_per_sessie']
 
     dagen_per_maand = 30
-    maanden = 144
-    vermogen_per_sessie = vermogen / 2
+    g8 = (50 * vermogen) / 12
+    k8 = g8 / 0.35
 
-    energiehoeveelheid = laadsessies_per_dag * dagen_per_maand * vermogen_per_sessie
-    omzet = laadsessies_per_dag * dagen_per_maand * (starttarief + parkeertarief + vermogen_per_sessie * kwh_prijs)
-    energiekost = energiehoeveelheid * kostprijs_energie
-    capaciteitskost = vermogen * 0.98
+    sessies_rij_46 = []
+    sessies = initiële_laadsessies
+    for _ in range(144):
+        sessies_rij_46.append(sessies)
+        sessies *= (1 + maandgroei)
+
+    maandwinsten = []
+    for maand in range(144):
+        sessies_dag = sessies_rij_46[maand]
+        sessies_maand = sessies_dag * dagen_per_maand
+        energie = sessies_maand * verbruik_per_sessie
+
+        omzet = (
+            sessies_dag * 30 * verbruik_per_sessie * kwhprijs +
+            (starttarief + parkeertarief) * sessies_dag * 30
+        )
+
+        energiekost = energie * kostprijsenergie
+        capaciteitskost = g8 if energie > k8 else energie * 0.35
+        huur = huurprijs if maand < 60 else huurprijs_later
+        winst = omzet - energiekost - capaciteitskost - huur
+
+        maandwinsten.append(winst)
 
     grafiek_data = []
-    cumulatief = -opstartkost
-    maand_sessies = laadsessies_per_dag
+    cumulatief = maandwinsten[0] - opstartkost
+    grafiek_data.append(cumulatief)
 
-    for maand in range(maanden):
-        sessies = maand_sessies * dagen_per_maand
-        energie = sessies * vermogen_per_sessie
-        omzet_m = sessies * (starttarief + parkeertarief + vermogen_per_sessie * kwh_prijs)
-        energiekost_m = energie * kostprijs_energie
-        capaciteit_m = vermogen * 0.98
-        winst = omzet_m - energiekost_m - capaciteit_m - huurprijs
-        cumulatief += winst
+    for i in range(1, 144):
+        cumulatief += maandwinsten[i]
         grafiek_data.append(cumulatief)
-        maand_sessies *= (1 + maandgroei)
 
-    rendement_maand_61 = grafiek_data[60] if len(grafiek_data) > 60 else 0
-    rendement_12jaar = grafiek_data[-1]
-    max_verlies = min(grafiek_data)
-    terugverdentijd = next((i + 1 for i, val in enumerate(grafiek_data) if val >= 0), maanden)
-    rendement_jaarlijks = ((rendement_12jaar + opstartkost) / opstartkost) ** (1 / 12) - 1
+    rendement_jaarlijks_lineair = round(
+        (grafiek_data[-1]) / (opstartkost + (huurprijs * 60)) / 12 * 100, 2
+    )
 
     return {
-        'energiehoeveelheid': energiehoeveelheid,
-        'omzet': omzet,
-        'energiekost': energiekost,
-        'capaciteitskost': capaciteitskost,
-        'huurprijs': huurprijs,
-        'grafiek_data': grafiek_data,
-        'rendement_maand_61': rendement_maand_61,
-        'rendement_12jaar': rendement_12jaar,
-        'max_verlies': max_verlies,
-        'terugverdentijd': terugverdentijd,
-        'rendement_jaarlijks': rendement_jaarlijks * 100
+        'grafiek_data': [round(x, 2) for x in grafiek_data],
+        'energiehoeveelheid': round(sessies_rij_46[0] * dagen_per_maand * verbruik_per_sessie, 2),
+        'energiekost': round((sessies_rij_46[0] * dagen_per_maand * verbruik_per_sessie) * kostprijsenergie, 2),
+        'capaciteitskost': round(g8 if (sessies_rij_46[0] * dagen_per_maand * verbruik_per_sessie) > k8 else (sessies_rij_46[0] * dagen_per_maand * verbruik_per_sessie * 0.35), 2),
+        'huurprijs': round(huurprijs, 2),
+        'omzet': round(
+            (sessies_rij_46[0] * 30 * verbruik_per_sessie * kwhprijs) +
+            (starttarief + parkeertarief) * sessies_rij_46[0] * 30,
+        2),
+        'rendement_maand_61': round(maandwinsten[60], 2),
+        'rendement_12jaar': round(grafiek_data[-1], 2),
+        'max_verlies': round(min(grafiek_data), 2),
+        'terugverdentijd': next((i + 1 for i, val in enumerate(grafiek_data) if val >= 0), 144),
+        'rendement_jaarlijks': rendement_jaarlijks_lineair
     }
